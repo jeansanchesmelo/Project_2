a7047d4ec7b9a6c7dbd24a4d7cc98442c870e46b
Jean Melo <jeansanchesmelo@live.com>
2022-03-10 20:36:30 -0300

    Project 2 - fourteenth commit

---
+++ src/main/java/com/projectone/projecttwo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.projectone.projecttwo.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.projectone.projecttwo.domain.Post;
+import com.projectone.projecttwo.repository.PostRepository;
+import com.projectone.projecttwo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		
+		Optional<Post> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
