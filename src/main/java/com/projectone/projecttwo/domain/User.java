8ac8710ef54f98e88f6b0f280ca3b5d9afa24479
Jean Melo <jeansanchesmelo@live.com>
2022-03-10 20:09:38 -0300

    Project 2 - twelfth commit

--- src/main/java/com/projectone/projecttwo/domain/User.java
+++ src/main/java/com/projectone/projecttwo/domain/User.java
@@ -1,7 +1,10 @@
 package com.projectone.projecttwo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -12,6 +15,8 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -54,9 +59,20 @@ public class User implements Serializable {
 		
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
+		
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
