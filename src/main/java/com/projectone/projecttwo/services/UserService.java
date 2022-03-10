425ed7fc892182c8e2d31fd3052956e9f34ed9f3
Jean Melo <jeansanchesmelo@live.com>
2022-03-10 13:28:25 -0300

    Project 2 - sixth commit

--- src/main/java/com/projectone/projecttwo/services/UserService.java
+++ src/main/java/com/projectone/projecttwo/services/UserService.java
@@ -1,10 +1,12 @@
 package com.projectone.projecttwo.services;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.projectone.projecttwo.domain.User;
 import com.projectone.projecttwo.repository.UserRepository;
+import com.projectone.projecttwo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,6 +17,13 @@ public class UserService {
 	public List<User> findAll() {
 		
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
---
+++ src/main/java/com/projectone/projecttwo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.projectone.projecttwo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		
+	}
+	
+}
