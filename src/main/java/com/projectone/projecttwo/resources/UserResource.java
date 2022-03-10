9c44f3a2446bdf8730b76da578f3d83d1cddad4d
Jean Melo <jeansanchesmelo@live.com>
2022-03-10 12:38:53 -0300

    Project 2 - fifth commit

--- src/main/java/com/projectone/projecttwo/resources/UserResource.java
+++ src/main/java/com/projectone/projecttwo/resources/UserResource.java
@@ -1,12 +1,14 @@
 package com.projectone.projecttwo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.projectone.projecttwo.domain.User;
+import com.projectone.projecttwo.dto.UserDTO;
 import com.projectone.projecttwo.services.UserService;
 
 @RestController
@@ -17,11 +19,12 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		
 		List<User> list = service.findAll();
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 				
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
