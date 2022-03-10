c9e950a8ee02435f9f83285d1bc408ffd8d19382
Jean Melo <jeansanchesmelo@live.com>
2022-03-10 20:17:31 -0300

    Project 2 - thirteenth commit

--- src/main/java/com/projectone/projecttwo/resources/UserResource.java
+++ src/main/java/com/projectone/projecttwo/resources/UserResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import com.projectone.projecttwo.domain.Post;
 import com.projectone.projecttwo.domain.User;
 import com.projectone.projecttwo.dto.UserDTO;
 import com.projectone.projecttwo.services.UserService;
@@ -65,6 +66,14 @@ public class UserResource {
 		obj = service.update(obj);
 				
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);		
+						
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
